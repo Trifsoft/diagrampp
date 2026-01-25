3dd4dca2fd5b8b9356398a30a4207e15e87ab03c
filip <djurkovicfilip03@gmail.com>
2026-01-25 22:01:34 +0100

    fixed GUI bug

--- include/view/cpp_class.h
+++ include/view/cpp_class.h
@@ -32,7 +32,6 @@ public:
     void addLineConnection(CppClass* target, BranchType branch);
     void addConnectionInfo(QGraphicsLineItem* line, bool isStart);
     QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
-    void updateAllConnections();
     virtual Composition* get_uml_class_diagram_node() override;
 
 private:
@@ -50,6 +49,7 @@ private:
 
     void updateConnectionLine(Connection& conn);
     void updateConnectionArrow(Connection& conn);
+    void updateAllConnections(int level);
     QGraphicsPolygonItem* getArrow(BranchType branchType);
 
     Board* m_board;
