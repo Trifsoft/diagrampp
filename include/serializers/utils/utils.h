d280835ab94e5692049a9398f64eaa599441c87e
David Curuvija <david.curuvija@yahoo.com>
2026-02-01 14:09:16 +0100

    Added helper functions for serializing

--- include/serializers/utils/utils.h
+++ include/serializers/utils/utils.h
@@ -2,9 +2,18 @@
 #define UTILS_H
 
 #include <ostream>
+#include <functional>
 
 namespace SerializeHelpers{
     void indent(std::ostream& output_stream, const int indentation_counter);
+
+    void write_indented_string_field(std::ostream& output_stream, const int indentation_counter, const std::string& key, const std::string& value);
+
+    template <typename T>
+    void write_indented_field(std::ostream& output_stream, const int indentation_counter, const std::string& key, const T& value);
+
+    template <typename T>
+    void write_indented_serialized_field(std::ostream& output_stream, const int indentation_counter, const std::string& key, T* value, std::function<void(std::ostream&, const int, T*)> serializer);
 }
 
 #endif // UTILS_H
--- src/serializers/utils/utils.cpp
+++ src/serializers/utils/utils.cpp
@@ -5,3 +5,31 @@ void SerializeHelpers::indent(std::ostream& output_stream, const int indentation
         output_stream << "\t";
     }
 }
+
+void write_with_quotes(std::ostream& output_stream, const std::string& text) {
+    output_stream << '"' << text << "\": ";
+}
+
+void SerializeHelpers::write_indented_string_field(std::ostream& output_stream, const int indentation_counter, const std::string& key, const std::string& value) {
+    SerializeHelpers::indent(output_stream, indentation_counter);
+    write_with_quotes(output_stream, key);
+    output_stream << ": ";
+    write_with_quotes(output_stream, value);
+}
+
+template <typename T>
+void SerializeHelpers::write_indented_field(std::ostream& output_stream, const int indentation_counter, const std::string& key, const T& value) {
+    SerializeHelpers::indent(output_stream, indentation_counter);
+    write_with_quotes(output_stream, key);
+    output_stream << ": " << value;
+}
+
+template <typename T>
+void SerializeHelpers::write_indented_serialized_field(std::ostream& output_stream, const int indentation_counter, const std::string& key, T* value, std::function<void(std::ostream&, const int, T*)> serializer) {
+    SerializeHelpers::indent(output_stream, indentation_counter);
+    write_with_quotes(output_stream, key);
+    output_stream << ": ";
+
+    serializer(output_stream, indentation_counter+1, value);
+
+}
